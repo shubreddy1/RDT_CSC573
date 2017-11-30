00a1f0257493d14a97c11268ebd53b536e29c671
shubreddy <shubreddy01@gmail.com>
2017-11-29 23:33:17 -0500

    modified:   client.py 	modified:   server.py

--- client.py
+++ client.py
@@ -9,86 +9,45 @@ from time import sleep
 import thread
 import hashlib
 import signal
-
-def handler(signum,frame):
-	raise IOError
-
-dval="""def my_handler(ip,port,filename,mss):
-	print "started"
-	signal.signal(signal.SIGALRM,handler)
-	irtt=0.001
-	diff=1
-	print "executing"
-	clientSocket = socket(AF_INET, SOCK_STREAM)
-	clientSocket.connect((ip,port))
-	f=open(filename,"rb")
-	kt=len(f.read())
-	segments=math.ceil(kt/500)
-	f.seek(0)
-	cont="ok"
-	seqno=1
-	m=hashlib.md5()
-	cont=f.read(500)
-	while cont:
-		ic="ok2"
-		#cont=""
-		
-		m.update(cont)
-		s=m.hexdigest()
-		s=s[-16:0]
-		print cont
-		header1=format(seqno,'032b')
-		header2=s
-		fl=0
-		header3="0101010101010101"
-		while fl==0:
-			try:
-				if seqno==segments:
-					clientSocket.send(header1+header2+header3+cont+"e_c01")
-				else:
-					clientSocket.send(header1+header2+header3+cont)
-				signal.alarm(irtt+diff/8)			
-				reply=clientSocket.recv(64)
-				rseq=reply[:16]
-				rseq=int(rseq,2)
-				if rseq==seqno:
-					fl=1
-			except IOError:
-				diff*=1.5
-				print "client timed out"
-		signal.alarm(0)
-		seqno+=1
-		cont=f.read(500)
-	print "finished transmitting"
-"""
-
-mss=500
-filename="rfc5.txt"
-serverlist = ['10.0.0.1','10.0.0.3']
-signal.signal(signal.SIGALRM,handler)
-serverPort = 12003 #default given port number
+import sys
+if len(sys.argv)<5:
+	print "insufficient arguments"
+	exit(1)
+serverlist=sys.argv[1:-3]
+mss=int(sys.argv[-1])
+filename=sys.argv[-2]
+serverPort=int(sys.argv[-3])
+#print serverlist,mss,filename,serverPort
+#mss=500
+#filename="rfc5.txt"
+#serverlist = ['10.0.0.1','10.0.0.3','10.0.0.4']
+#signal.signal(signal.SIGALRM,handler)
+#serverPort = 12003 #default given port number
 conarr=[]
+b=format(mss,'016b')
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 for ip in serverlist:
-	clientSocket = socket(AF_INET, SOCK_STREAM)
-	clientSocket.connect((ip,serverPort))
-	conarr.append(clientSocket)
+	cspair=(ip,serverPort)
+	conarr.append(cspair)
+	clientSocket.sendto(b,cspair)
 fl2=0
 print "starting transmission"
-irtt=0.001
-diff=1
-print conarr
-print "executing part"
+irtt=0.01
+tarr=0
+diff=0.01
+#print conarr
+#print "executing part"
 #clientSocket = socket(AF_INET, SOCK_STREAM)
 #clientSocket.connect((ip,port))
 f=open(filename,"rb")
 kt=len(f.read())
-segments=math.ceil(kt/500)
+segments=math.ceil(kt/mss)
 f.seek(0)
 while fl2==0:		
 	cont="ok"
 	seqno=1
 	m=hashlib.md5()
-	cont=f.read(500)
+	cont=f.read(mss)
 	while cont!="":
 		#ic="ok2"
 		#cont=""
@@ -96,37 +55,38 @@ while fl2==0:
 		s=m.hexdigest()
 		s=s[0:-16]
 		#print "hexdigest is ",s
-		#print cont
+		#print cont[:20],cont[-20:0]
 		header1=format(seqno,'032b')
 		header2=s
-		
 		header3="0101010101010101"
-		for clientSocket in conarr:
+		for cspair in conarr:
+			#print cspair
 			fl=0
 			while fl==0:
 				try:
 					if seqno==segments+1:
-						clientSocket.send(header1+header2+header3+cont+"e_c01")
+						clientSocket.sendto(header1+header2+header3+cont+"e_c01",cspair)
 						fl2=1
 					else:
-						clientSocket.send(header1+header2+header3+cont)
-					tdtime=irtt+diff
-					tdtime=int(tdtime)
-					#print "alarm set for ",tdtime," seconds"
-					signal.alarm(tdtime)	
+						clientSocket.sendto(header1+header2+header3+cont,cspair)
+					tdtime=irtt
+					clientSocket.settimeout(tdtime)	
 					#print "waiting for reply"		
-					reply=clientSocket.recv(64)
+					reply,tarr=clientSocket.recvfrom(64)
 					rseq=reply[:32]
+					#print rseq
 					rseq=int(rseq,2)
 					if rseq==seqno:
 						fl=1
 						diff=max(1,diff-1)
-				except IOError:
-					diff=1
-					print "client timed out"
-			signal.alarm(0)
+					#irtt=irtt*0.85
+				except timeout:
+					#irtt=irtt*2
+					t_var=0
+					#print "Timeout, sequence number = ",seqno
+				clientSocket.settimeout(None)
 		seqno+=1
-		cont=f.read(500)
+		cont=f.read(mss)
 		#print cont[:50]
 	print "finished transmitting"
 
--- server.py
+++ server.py
@@ -6,54 +6,76 @@ import thread
 from threading import Thread
 from time import sleep
 import datetime
+import time
 from random import randint
 import hashlib
+import sys
 
-prob=0.05
-l=len(str(prob))
-for x in range(2,l):
-	prob*=10
-filename='rfc2.txt'
+if len(sys.argv)<4:
+	print "insufficient arguments"
+	exit(1)
+port = int(sys.argv[1])
+filename=sys.argv[2]
+prob=float(sys.argv[3])
+#print port,filename,prob
+#prob=0.05
+prob*=100
+prob=int(prob)
+print prob
+#filename='rfc2.txt'
 #signal.signal(signal.SIGALRM,handler)
-serverPort = 12003
-serverSocket = socket(AF_INET,SOCK_STREAM)
+#serverPort = 12003
+serverSocket = socket(AF_INET,SOCK_DGRAM)
 serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-serverSocket.bind(("10.0.0.1",12003))
-serverSocket.listen(1)
-connectionSocket, addr = serverSocket.accept()
-sentence = connectionSocket.recv(1024+64)
+serverSocket.bind(("10.0.0.3",port)) #type in the IP of the PC on which the server is executing
+sentence,addr = serverSocket.recvfrom(16)
+mss=int(sentence,2)
+print mss
+sentence,addr = serverSocket.recvfrom(mss+64)
+cspair=addr
+tarr=""
 seq=1
 f=open(filename,'wb')
+start_time=time.time()
 while "e_c01" not in sentence:
 	#print seq,"      ",sentence[:100]
 	l=randint(0,100)
 	header1=sentence[:32]
 	#print header1
 	header1=str(header1)
+	#print header1
 	hd1=int(header1,2)
 	header1=hd1
 	header2=sentence[32:48]
+	#print header1,seq,header2
 	header3=sentence[48:64]
 	sentence=sentence[64:]
 	#print "checking packet"
 	if l>prob and seq==header1:
-		print "wrote packet ",seq
+		#print "wrote packet ",seq
 		f.write(sentence)
 		header1=format(seq,'032b')
 		header2="0000000000000000"
 		header3="1010101010101010"
 		seq+=1
-		connectionSocket.send(header1+header2+header3)	
+		serverSocket.sendto(header1+header2+header3,cspair)	
 	else:
-		print "discarded packet ",seq, " cause ",l,"less than ",prob
-	sentence=connectionSocket.recv(1024+64)
+		if l<prob:
+			t_var=0
+			#print "Packet Loss, sequence number = ",seq
+		else:
+			t_var=0
+			#print "early client timeout"
+	sentence,tarr=serverSocket.recvfrom(mss+64)
 #print sentence
 header1=format(seq,'032b')
 header2="0000000000000000"
 header3="1010101010101010"
 seq+=1
-connectionSocket.send(header1+header2+header3)
+serverSocket.sendto(header1+header2+header3,cspair)
 sentence=sentence[64:-5]
 f.write(sentence)
-connectionSocket.close()
+finish_time=time.time()
+print "total time taken ",finish_time-start_time
+
 
